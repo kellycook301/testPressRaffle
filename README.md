16f4adbec8ca76886b292fccd2a1c3a3681574d6
Kelly Cook <kellycook20878@gmail.com>
2020-06-02 11:21:57 -0500

    made raffle winner picker

---
+++ README.md
@@ -0,0 +1,3 @@
+# Thirty Nights of Violence Test Press/Merch Raffle
+World's most simple program to pick the winner of a raffle that my band did to raise money for
+Black Lives Matter Global Network
---
+++ raffle.js
@@ -0,0 +1,38 @@
+let raffleEntries = [
+    'kellycook301',
+    'jordenhart',
+    'ianjaredbell',
+    'grantth7',
+    'elxchoppo',
+    'early_humans',
+    '_alexdejong',
+    'onlinedungeon',
+    'dontcallmevince',
+    'tosinabasi',
+    'detroitarun',
+    'elxchoppo',
+    'tatemercer',
+    'youngfratjesus',
+    'bertkreischer',
+    'andrewvle',
+    'florencepugh',
+    'streetprowl_abe',
+    'm_shinoda',
+    'jordenhart',
+    'detroitarun',
+    'grantth7',
+    'kellycook301',
+    'jordenhart',
+];
+
+let winner = raffleEntries[Math.floor(Math.random() * raffleEntries.length)];
+
+console.log("")
+console.log("")
+console.log("")
+console.log("Congrats to " + winner + " for winning a test press, a wall flag, and a shirt.")
+console.log("Please contact us for your shirt choice.")
+console.log("MORE IMPORTANTLY, thank you to everyone who entered for contributing to Black Lives Matter Global Network.")
+console.log("")
+console.log("")
+console.log("")
