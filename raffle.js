a436e6bb3f351cd65dd24eb1f28226bc53607c4c
Kelly Cook <kellycook20878@gmail.com>
2020-06-02 20:18:17 -0500

    front-end presentation

--- raffle.js
+++ raffle.js
@@ -1,38 +1,31 @@
-let raffleEntries = [
-    'kellycook301',
-    'jordenhart',
-    'ianjaredbell',
-    'grantth7',
-    'elxchoppo',
-    'early_humans',
-    '_alexdejong',
-    'onlinedungeon',
-    'dontcallmevince',
-    'tosinabasi',
-    'detroitarun',
-    'elxchoppo',
-    'tatemercer',
-    'youngfratjesus',
-    'bertkreischer',
-    'andrewvle',
-    'florencepugh',
-    'streetprowl_abe',
-    'm_shinoda',
-    'jordenhart',
-    'detroitarun',
-    'grantth7',
-    'kellycook301',
-    'jordenhart',
-];
-
-let winner = raffleEntries[Math.floor(Math.random() * raffleEntries.length)];
-
-console.log("")
-console.log("")
-console.log("")
-console.log("Congrats to " + winner + " for winning a test press, a wall flag, and a shirt.")
-console.log("Please contact us for your shirt choice.")
-console.log("MORE IMPORTANTLY, thank you to everyone who entered for contributing to Black Lives Matter Global Network.")
-console.log("")
-console.log("")
-console.log("")
+function getRaffleWinner() {
+    var raffleEntries = new Array(
+    '@kellycook301',
+    '@jordenhart',
+    '@ianjaredbell',
+    '@grantth7',
+    '@elxchoppo',
+    '@early_humans',
+    '@_alexdejong',
+    '@onlinedungeon',
+    '@dontcallmevince',
+    '@tosinabasi',
+    '@detroitarun',
+    '@elxchoppo',
+    '@tatemercer',
+    '@youngfratjesus',
+    '@bertkreischer',
+    '@andrewvle',
+    '@florencepugh',
+    '@streetprowl_abe',
+    '@m_shinoda',
+    '@jordenhart',
+    '@detroitarun',
+    '@grantth7',
+    '@kellycook301',
+    '@jordenhart'
+    );
+    var random = raffleEntries[Math.floor(Math.random() * raffleEntries.length)];
+    var text = "Congrats to " + random + " for winning a test press, a wall flag, and a shirt. Please contact us for your shirt choice. MORE IMPORTANTLY, thank you to everyone who entered for contributing to Black Lives Matter Global Network."
+    document.getElementById("message").innerHTML=text;
+}
